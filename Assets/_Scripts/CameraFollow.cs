4166d1faf64966121297fb9c896ee6fea0313cd1
AG <adigaash@gmail.com>
2026-04-23 06:18:06 +0300

    Adding some assets to start with level generation

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform playerTransform;
+
+     void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+    public class PlayerControl : MonoBehaviour
+    {
+
+        Transform playerTransform;
+         
+        void Awake()
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        
+        public void MoveUp()
+        {
+            playerTransform.position += Vector3.up;
+        }
+    }
