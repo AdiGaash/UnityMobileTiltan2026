009fe6f707bde59938d9ed2e82db3336ae1bf3ef
AG <adigaash@gmail.com>
2026-05-06 22:56:11 +0300

    score manager

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+    [SerializeField] private IntVariable scoreVariable;
+
+    public UnityEvent<int> OnScoreUpdated = new UnityEvent<int>();
+
+    private void Start()
+    {
+        // Subscribe to score changes from the IntVariable
+        if (scoreVariable != null)
+        {
+            scoreVariable.OnValueChanged += HandleScoreChanged;
+        }
+        else
+        {
+            Debug.LogWarning("ScoreManager: scoreVariable is not assigned!");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe to prevent memory leaks
+        if (scoreVariable != null)
+        {
+            scoreVariable.OnValueChanged -= HandleScoreChanged;
+        }
+    }
+
+    
+    private void HandleScoreChanged(int newScore)
+    {
+        OnScoreUpdated.Invoke(newScore);
+        Debug.Log($"Score updated to: {newScore}");
+    }
+}
