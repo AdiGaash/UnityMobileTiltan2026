e6aea80a9625a978c4f69d54fa774d99a13c6fe5
AG <adigaash@gmail.com>
2026-05-06 22:00:01 +0300

    intver SO

---
+++ Assets/_Scripts/Generic/IntVariable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "IntVariable", menuName = "ScriptableObjects/IntVariable")]
+public class IntVariable : ScriptableObject
+{
+    [SerializeField] private int value;
+
+    public event UnityAction<int> OnValueChanged = delegate { };
+    public int Value
+    {
+        get => value;
+        set
+        {
+            this.value = value;
+            OnValueChanged.Invoke(value);
+        }
+    }
+}
