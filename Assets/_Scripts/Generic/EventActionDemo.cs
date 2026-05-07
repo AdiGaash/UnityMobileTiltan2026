d1605ad2944d3f8d74fc0abb7c559b0341b2caf8
AG <adigaash@gmail.com>
2026-05-07 12:38:35 +0300

    adding events and unity events demo scripts

---
+++ Assets/_Scripts/Generic/EventActionDemo.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+
+public class EventActionDemo : MonoBehaviour
+{
+    // C# event declaration
+    public event Action OnPlayerDied;
+
+    public int health = 100;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        Debug.Log("Player took damage");
+
+        // Check if player died
+        if (health <= 0)
+        {
+            Debug.Log("Player died");
+
+            // Raise the event safely
+            OnPlayerDied?.Invoke();
+        }
+    }
+}
+
+
+public class EventActionListenerDemo : MonoBehaviour
+{
+    // Reference to the broadcaster
+    [SerializeField]
+    private EventActionDemo eventActionDemo;
+
+    // Subscribe when object becomes active
+    private void OnEnable()
+    {
+        eventActionDemo.OnPlayerDied += ShowGameOverScreen;
+    }
+
+    // Unsubscribe when object becomes inactive
+    private void OnDisable()
+    {
+        eventActionDemo.OnPlayerDied -= ShowGameOverScreen;
+    }
+
+    private void ShowGameOverScreen()
+    {
+        Debug.Log("Game Over Screen Opened");
+
+        // Example:
+        // gameOverPanel.SetActive(true);
+    }
+}
---
+++ Assets/_Scripts/UnityEventDemo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class UnityEventDemo : MonoBehaviour
+{
+    public UnityEvent OnDamaged;
+
+    public void TakeDamage()
+    {
+        OnDamaged.Invoke();
+    }
+}
