7886ab2ccba92013eb315aa086c0e69f1948e173
AG <adigaash@gmail.com>
2026-05-07 12:14:14 +0300

    start coroutine

---
+++ Assets/_Scripts/StartCoroutine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class StartCoroutine : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        Debug.Log("Game Starting");
+
+        yield return new WaitForSeconds(2f);
+
+        Debug.Log("2 seconds later");
+    }
+}
