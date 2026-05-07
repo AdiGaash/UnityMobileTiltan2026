0ef527bba97fc76a747af1458f999760a985ccdc
AG <adigaash@gmail.com>
2026-05-07 13:56:59 +0300

    simple SOeventref

---
+++ Assets/_Scripts/ItemCollectedEventSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "Events/Item Collected Event")]
+public class ItemCollectedEventSO : ScriptableObject
+{
+    // Event with no parameters
+    public event Action OnRaised;
+
+    // Method to trigger event
+    public void Raise()
+    {
+        OnRaised?.Invoke();
+    }
+}
