591fce3f96333656b1346a6c82944af03ac44ef7
AG <adigaash@gmail.com>
2026-06-04 14:43:23 +0300

    adding capture camera

---
+++ Assets/_Scripts/WebCamBasic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WebCamBasic : MonoBehaviour
+    {
+        public RawImage rawImage;
+        private WebCamTexture webCamTexture;
+        WebCamDevice[] devices;
+        
+        private void Start()
+        {
+            devices = WebCamTexture.devices;
+            if (devices.Length > 0)
+            {
+                WebCamDevice device = devices[0]; // Use the first available camera
+                webCamTexture = new WebCamTexture(device.name);
+                rawImage.texture = webCamTexture;
+                webCamTexture.Play();   
+            }
+        }
+    }
