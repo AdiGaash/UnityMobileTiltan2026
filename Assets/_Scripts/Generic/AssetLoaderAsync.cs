3249f422f6f86600e0bc9775c06a728eedbb4fb0
AG <adigaash@gmail.com>
2026-06-11 14:08:17 +0300

    async opration

---
+++ Assets/_Scripts/Generic/AssetLoaderAsync.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetLoaderAsync : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        ResourceRequest request = Resources.LoadAsync<GameObject>("Enemy");
+
+        yield return request;
+
+        GameObject enemyPrefab = request.asset as GameObject;
+
+        Instantiate(enemyPrefab);
+    }
+}
