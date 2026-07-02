83a8257431ba2fe37a2932c6773a01964d99bf90
AG <adigaash@gmail.com>
2026-07-02 13:26:24 +0300

    animation curve SO

---
+++ Assets/_Scripts/DifficultyCurve.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Difficulty Curve", menuName = "ScriptableObjects/DifficultyCurve", order = 1)]
+public class DifficultyCurve : ScriptableObject
+{
+    public AnimationCurve difficultyOverTime;
+    public float initialDifficulty = 1f;
+    public float timeMultiplier = 1.0f;
+
+    private float currentTime = 0f;
+
+    public void ResetTime()
+    {
+        currentTime = 0f;
+    }
+
+    private void OnEnable()
+    {
+        difficultyOverTime.postWrapMode = WrapMode.ClampForever;
+    }
+
+    public float GetDifficulty(float deltaTime)
+    {
+        currentTime += deltaTime * timeMultiplier;
+        currentTime = Mathf.Clamp(currentTime, 0f, initialDifficulty);
+        return difficultyOverTime.Evaluate(currentTime);
+    }
+}
